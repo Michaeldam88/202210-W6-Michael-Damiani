75742113d4bb166ce756b2dcd0ba40012d187434
Michaeldam88 <michaeldam88@gmail.com>
2022-11-12 21:05:16 +0100

    added the function newPhase and checkCase

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const createTable = (tableSize) => {
-    const gameTable = [];
-    for (let i = 0; i < tableSize; i++) {
-        const newRow = [];
-        for (let i = 0; i < tableSize; i++) {
-            newRow.push(0);
-        }
-        gameTable.push(newRow);
-    }
-    return gameTable;
-};
+import { createTable } from './js/createTable.js';
+import { newPhase } from './js/newPhase.js';
 
-const gameTable = createTable(3);
+const tableSize = 4;
+let gameTable = createTable(tableSize);
 
+//asignación de punto de inicio manual para testear
+gameTable[0][1] = 1;
+gameTable[1][1] = 1;
+gameTable[2][1] = 1;
+
+console.table(gameTable);
+gameTable = newPhase(gameTable);
 console.table(gameTable);
---
+++ js/checkCase.js
@@ -0,0 +1,38 @@
+export const checkCase = (element, rowI, columnI, gameTable) => {
+    let liveElements;
+    let rowPlus = rowI + 1;
+    let rowLess = rowI - 1;
+    let columnPlus = columnI + 1;
+    let columnLess = columnI + 1;
+
+    if (gameTable[rowLess] === undefined) {
+        rowLess = gameTable.length - 1;
+    }
+    if (gameTable[columnLess] === undefined) {
+        columnLess = gameTable[rowI].length - 1;
+    }
+    if (gameTable[rowPlus] === undefined) {
+        rowPlus = gameTable[0];
+    }
+    if (gameTable[columnPlus] === undefined) {
+        columnPlus = gameTable[rowI][0];
+    }
+
+    liveElements += gameTable[rowI][columnPlus];
+    liveElements += gameTable[rowI][columnLess];
+
+    liveElements += gameTable[rowLess][columnLess];
+    liveElements += gameTable[rowLess][columnI];
+    liveElements += gameTable[rowLess][columnPlus];
+
+    liveElements += gameTable[rowPlus][columnLess];
+    liveElements += gameTable[rowPlus][columnI];
+    liveElements += gameTable[rowPlus][columnPlus];
+    if (element === 1) {
+        if (liveElements > 3) return 0;
+        if (liveElements < 2) return 0;
+        return 1;
+    }
+    if (liveElements === 3) return 0;
+    return 0;
+};
