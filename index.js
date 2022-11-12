8eb3f63af38f8ca167992146462ab5be784a8194
Michaeldam88 <michaeldam88@gmail.com>
2022-11-12 17:55:29 +0100

    create the new table function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const createTable = (tableSize) => {
+    const gameTable = [];
+    for (let i = 0; i < tableSize; i++) {
+        const newRow = [];
+        for (let i = 0; i < tableSize; i++) {
+            newRow.push(0);
+        }
+        gameTable.push(newRow);
+    }
+    return gameTable;
+};
 
+const gameTable = createTable(3);
+
+console.table(gameTable);
--- readme.md
+++ readme.md
@@ -3,3 +3,6 @@
 # Challege Week 6
 
 ## Conway's Game of Life
+
+
+https://sonarcloud.io/project/configuration?id=Michaeldam88_202210-W6-Michael-Damiani
