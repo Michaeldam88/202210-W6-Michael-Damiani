601f6b9c76392ef8d0cc1dff0e8f2b6116f7ce59
Michaeldam88 <michaeldam88@gmail.com>
2022-11-13 18:06:53 +0100

    added the start and stop functionality

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 import { createTable } from './js/createTable.js';
 import { newPhase } from './js/newPhase.js';
 
-const tableSize = 30;
+const startSimulation = document.getElementById('start');
+const stopSimulation = document.getElementById('stop');
+const tableSize = document.getElementById('tableSize');
 
-let gameTable = createTable(tableSize);
-
-//asignación de punto de inicio manual para testear
-gameTable[1][2] = 1;
-gameTable[2][2] = 1;
-gameTable[3][2] = 1;
-gameTable[4][2] = 1;
-gameTable[5][2] = 1;
+let tableUpdate;
+let gameTable;
 
 const showNewTable = () => {
     gameTable = newPhase(gameTable);
     console.table(gameTable);
 };
 
-setInterval(showNewTable, 1000);
+startSimulation.addEventListener('click', () => {
+    gameTable = createTable(tableSize.value);
+
+    //asignación de punto de inicio manual para testear
+    gameTable[1][2] = 1;
+    gameTable[2][2] = 1;
+    gameTable[3][2] = 1;
+    gameTable[4][2] = 1;
+    gameTable[5][2] = 1;
+    tableUpdate = setInterval(showNewTable, 1000);
+});
+
+stopSimulation.addEventListener('click', () => {
+    window.clearInterval(tableUpdate);
+});
