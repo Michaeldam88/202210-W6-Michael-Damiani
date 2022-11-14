d31b1e5a568087482b30bc59234db47b6335c7ab
Michaeldam88 <michaeldam88@gmail.com>
2022-11-14 17:17:49 +0100

    added graphic display

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 import { createTable } from './js/createTable.js';
 import { newPhase } from './js/newPhase.js';
+import { generateRandomTable } from './js/randomTable.js';
 
 const startSimulation = document.getElementById('start');
 const stopSimulation = document.getElementById('stop');
 const tableSize = document.getElementById('tableSize');
+const liveCells = document.getElementById('liveCells');
 
 let tableUpdate;
 let gameTable;
 
 const showNewTable = () => {
-    gameTable = newPhase(gameTable);
-    console.table(gameTable);
+    gameTable = newPhase(gameTable);    
 };
 
 startSimulation.addEventListener('click', () => {
     gameTable = createTable(tableSize.value);
-
-    //asignación de punto de inicio manual para testear
-    gameTable[1][2] = 1;
-    gameTable[2][2] = 1;
-    gameTable[3][2] = 1;
-    gameTable[4][2] = 1;
-    gameTable[5][2] = 1;
+    gameTable = generateRandomTable(gameTable, liveCells.value);    
     tableUpdate = setInterval(showNewTable, 1000);
 });
 
--- js/newPhase.js
+++ js/newPhase.js
@@ -1,12 +1,27 @@
 import { checkCase } from './checkCase.js';
+const displayContainerDiv = document.getElementById('displayContainer');
+
 export const newPhase = (gameTable) => {
     const newTableSituation = [];
+    displayContainerDiv.innerHTML = '';
     gameTable.forEach((row, rowI) => {
+        const newRowDiv = document.createElement('div');
+        newRowDiv.className = 'newRowDiv';
         const newRow = [];
         row.forEach((element, columnI) => {
-            newRow.push(checkCase(element, rowI, columnI, gameTable));
+            const checkResult = checkCase(element, rowI, columnI, gameTable);
+            newRow.push(checkResult);
+
+            const newDivElement = document.createElement('div');
+            if (checkResult === 1) {
+                newDivElement.className = 'liveCells';
+            } else {
+                newDivElement.className = 'deadCells';
+            }
+            newRowDiv.appendChild(newDivElement);
         });
         newTableSituation.push(newRow);
+        displayContainerDiv.appendChild(newRowDiv);
     });
     return newTableSituation;
 };
