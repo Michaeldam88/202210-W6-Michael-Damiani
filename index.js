b6c723ba985949a7314dfa392b3eca8a505f884b
Michaeldam88 <michaeldam88@gmail.com>
2022-11-14 20:50:07 +0100

    fixed some problem

--- index.js
+++ index.js
@@ -3,23 +3,35 @@ import { newPhase } from './js/newPhase.js';
 import { generateRandomTable } from './js/randomTable.js';
 
 const startSimulation = document.getElementById('start');
-const stopSimulation = document.getElementById('stop');
+const stopSimulationBtn = document.getElementById('stop');
 const tableSize = document.getElementById('tableSize');
 const liveCells = document.getElementById('liveCells');
+const refreshTime = document.getElementById('refreshTime');
+const turnsDisplay = document.getElementById('turnsDisplay');
+const displayContainerDiv = document.getElementById('displayContainer');
 
 let tableUpdate;
 let gameTable;
+let turnNumber;
 
 const showNewTable = () => {
-    gameTable = newPhase(gameTable);    
+    gameTable = newPhase(gameTable);
+    if (gameTable === 0) {
+        displayContainerDiv.textContent = 'No Alive Cells';
+        window.clearInterval(tableUpdate);
+    }
+    turnNumber++;
+    turnsDisplay.value = turnNumber;
 };
 
 startSimulation.addEventListener('click', () => {
     gameTable = createTable(tableSize.value);
-    gameTable = generateRandomTable(gameTable, liveCells.value);    
-    tableUpdate = setInterval(showNewTable, 1000);
+    gameTable = generateRandomTable(gameTable, liveCells.value);
+    tableUpdate = setInterval(showNewTable, refreshTime.value * 1000);
+    turnNumber = 0;
+    turnsDisplay.value = turnNumber;
 });
 
-stopSimulation.addEventListener('click', () => {
+stopSimulationBtn.addEventListener('click', () => {
     window.clearInterval(tableUpdate);
 });
--- js/newPhase.js
+++ js/newPhase.js
@@ -1,13 +1,17 @@
 import { checkCase } from './checkCase.js';
+
 const displayContainerDiv = document.getElementById('displayContainer');
 
 export const newPhase = (gameTable) => {
     const newTableSituation = [];
+    let aliveCells = 0;
     displayContainerDiv.innerHTML = '';
+
     gameTable.forEach((row, rowI) => {
         const newRowDiv = document.createElement('div');
         newRowDiv.className = 'newRowDiv';
         const newRow = [];
+
         row.forEach((element, columnI) => {
             const checkResult = checkCase(element, rowI, columnI, gameTable);
             newRow.push(checkResult);
@@ -15,13 +19,17 @@ export const newPhase = (gameTable) => {
             const newDivElement = document.createElement('div');
             if (checkResult === 1) {
                 newDivElement.className = 'liveCells';
+                aliveCells++;
             } else {
                 newDivElement.className = 'deadCells';
             }
             newRowDiv.appendChild(newDivElement);
         });
+
         newTableSituation.push(newRow);
         displayContainerDiv.appendChild(newRowDiv);
     });
+
+    if (aliveCells === 0) return 0;
     return newTableSituation;
 };
