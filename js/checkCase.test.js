66e8820c28fe49d611d135f7435d2393b0deaa83
Michaeldam88 <michaeldam88@gmail.com>
2022-11-15 11:08:44 +0100

    added the checcase test

---
+++ js/checkCase.test.js
@@ -0,0 +1,97 @@
+import { checkCase } from './checkCase.js';
+
+describe('Given a table of [[0, 0, 0],[0, 1, 1],[0, 0, 0]]', () => {
+    const gameTable = [
+        [0, 0, 0],
+        [0, 1, 1],
+        [0, 0, 0],
+    ];
+    const rowI = 1;
+    const columnI = 1;
+    const element = gameTable[rowI][columnI];
+    const check = checkCase(element, rowI, columnI, gameTable);
+
+    test(`if we check  the central cell it should return 0`, () => {
+        expect(check).toBe(0);
+    });
+});
+
+describe('Given a table of [[0, 0, 0],[1, 1, 1],[0, 0, 0]]', () => {
+    const gameTable = [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0],
+    ];
+    const rowI = 1;
+    const columnI = 1;
+    const element = gameTable[rowI][columnI];
+    const check = checkCase(element, rowI, columnI, gameTable);
+
+    test(`if we check  the central cell it should return 1`, () => {
+        expect(check).toBe(1);
+    });
+});
+
+describe('Given a table of [[0, 0, 0],[1, 1, 1],[0, 0, 0]]', () => {
+    const gameTable = [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0],
+    ];
+    const rowI = 0;
+    const columnI = 0;
+    const element = gameTable[rowI][columnI];
+    const check = checkCase(element, rowI, columnI, gameTable);
+
+    test(`if we check the first cell o the top left should return 1`, () => {
+        expect(check).toBe(1);
+    });
+});
+
+describe('Given a table of [[0, 0, 0],[0, 0, 0],[0, 0, 0]]', () => {
+    const gameTable = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ];
+    const rowI = 1;
+    const columnI = 1;
+    const element = gameTable[rowI][columnI];
+    const check = checkCase(element, rowI, columnI, gameTable);
+
+    test(`if we check  the central cell it should return 0`, () => {
+        expect(check).toBe(0);
+    });
+});
+
+describe('Given a table of [[0, 0, 0],[1, 1, 1],[0, 0, 0]]', () => {
+    const gameTable = [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0],
+    ];
+    const rowI = 2;
+    const columnI = 2;
+    const element = gameTable[rowI][columnI];
+    const check = checkCase(element, rowI, columnI, gameTable);
+
+    test(`if we check the first cell o the top left should return 1`, () => {
+        expect(check).toBe(1);
+    });
+});
+
+describe('Given a table of [[0, 1, 0],[1, 1, 1],[0, 1, 0]]', () => {
+    const gameTable = [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 1, 0],
+    ];
+    const rowI = 0;
+    const columnI = 1;
+    const element = gameTable[rowI][columnI];
+    const check = checkCase(element, rowI, columnI, gameTable);
+
+    test(`if we check the second cell o the top should return 0`, () => {
+        expect(check).toBe(0);
+    });
+});
