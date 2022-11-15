56c5ab7cc71abf19dfa2b7b2af4b4fd64c0ef8f3
Michaeldam88 <michaeldam88@gmail.com>
2022-11-15 10:41:05 +0100

    added the createTable test

---
+++ js/createTable.test.js
@@ -0,0 +1,14 @@
+import { createTable } from './createTable.js';
+
+describe('Given a value of 3', () => {
+    const result = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ];
+    const newTable = createTable(3);
+
+    test(`it should return [[0, 0, 0],[0, 0, 0],[0, 0, 0]]`, () => {
+        expect(newTable).toStrictEqual(result);
+    });
+});
