996323311f911f98f4fbef69566bdea5b9296ec3
Michaeldam88 <michaeldam88@gmail.com>
2022-11-17 16:17:08 +0100

    fixed infite loop of random table

--- __tests__/randomTable.test.js
+++ __tests__/randomTable.test.js
@@ -17,9 +17,23 @@ describe('Given a table of 3x3 and a live cell of 3', () => {
     test(`it should return 6`, () => {
         expect(result).toBe(6);
     });
-
-
 });
 
+describe('Given a table of 3x3 and a live cell of 15', () => {
+    const arr = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ];
+    const newTable = generateRandomTable(arr, 15);
+    let result = 0;
+    newTable.forEach((element) => {
+        element.forEach((el) => {
+            if (el === 1) result++;
+        });
+    });
 
-
+    test(`it should return 9`, () => {
+        expect(result).toBe(9);
+    });
+});
