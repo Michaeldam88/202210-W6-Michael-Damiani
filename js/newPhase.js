ddbc435f42d92cbd99c97dccab080819026d9942
Michaeldam88 <michaeldam88@gmail.com>
2022-11-17 17:03:49 +0100

    modified the name of the displayTable divs

--- js/newPhase.js
+++ js/newPhase.js
@@ -4,22 +4,18 @@ import { updateTable } from './updateTable.js';
 export const newPhase = (gameTable) => {
     let newTableSituation = [];
     let aliveCells = 0;
-
     gameTable.forEach((row, rowI) => {
         const newRow = [];
         row.forEach((element, columnI) => {
             const checkResult = checkCase(element, rowI, columnI, gameTable);
+            updateTable(rowI, columnI, checkResult);
             newRow.push(checkResult);
             if (checkResult === 1) {
                 aliveCells++;
             }
-            if (element !== checkResult) {
-                updateTable(rowI, columnI, checkResult);
-            }
         });
         newTableSituation.push(newRow);
     });
-
     if (aliveCells === 0) newTableSituation = [0];
     return newTableSituation;
 };
